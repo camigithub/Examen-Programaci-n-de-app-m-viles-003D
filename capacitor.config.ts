0348bacf6975ee292163943277e78c24969f7410
camigithub <120749029+camigithub@users.noreply.github.com>
2022-12-16 16:28:51 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.duoc.start',
+  appName: 'evaluacion1',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
